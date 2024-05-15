903f981596aa392f84577f954f28db7a157b2ad5
jngwk <jngwk.ch@gmail.com>
2024-05-15 23:53:36 +0900

    add react chapter_06

--- springboot_workspace/front/my-app/src/index.js
+++ springboot_workspace/front/my-app/src/index.js
@@ -9,30 +9,13 @@ import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
 import CommentList from "./chapter_05/CommentList";
 import AttendanceBook from "./chapter_10/AttendanceBook";
+import NotificationList from "./chapter_06/NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <NotificationList />
   </React.StrictMode>
 );
 
-// setInterval(() => {
-//   root.render(
-//     <React.StrictMode>
-//       <Clock />
-//     </React.StrictMode>
-//   );
-// }, 1000);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <CommentList />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
